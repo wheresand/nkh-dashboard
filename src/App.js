b2c39fb97a7f398f3db35e625c56d409c13aa1e4
wheresand <mysand33130@gmail.com>
2023-02-15 15:03:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import React from 'react';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+
+import Home from '/.components/Home';
+import About from '/.components/About';
+import contact from '/.components/contact';
+import content from '/.components/content';
+
 function App() {
   return (
+
     <div className="App">
-     <h1>nkh-dashboard</h1>
-     <button>buttonbootstrap</button>
-     <Button>buttonbootstrap</Button>
+        <Router basename="/">
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+
+          </Switch>
+        </Router>
     </div>
   );
 }
